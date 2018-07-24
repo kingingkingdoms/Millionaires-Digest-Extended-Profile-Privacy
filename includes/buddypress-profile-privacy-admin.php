6b891785501137a8d2b57f1582c841b9663d54c9
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-07-24 00:10:14 -0400

    Update buddypress-profile-privacy-admin.php

--- includes/buddypress-profile-privacy-admin.php
+++ includes/buddypress-profile-privacy-admin.php
@@ -1,25 +1,4 @@
 <?php
-/*
- * Setup admin notices for when updates are run. Used to announce new functions.
- */
-function sbpp04_update_notice__success() {
-    $notice_dismissed = get_user_option( SBPP04_ADMIN_NOTIFICATION_HIDDEN_KEY );
-    if( $notice_dismissed != 1 ){
-        $class = "notice notice-success is-dismissible sbpp04-notice";
-        $message = __( "A new admin setting has been added to Simple BuddyPress Profile Privacy. You can now allow members to hide themselves from the directory page. ", 'simple-buddypress-profile-privacy' );
-        $link_message = __( "Click here to update the profile settings", 'simple-buddypress-profile-privacy' );
-        printf( '<div class="%1s"><p>%2s<a href="' . get_site_url() . '/wp-admin/admin.php?page=bp-settings">%3s</a></p></div>', $class, $message, $link_message );
-    }
-}
-add_action( 'admin_notices', 'sbpp04_update_notice__success' );
-
-/*
- * Dismiss notice only once through AJAX
- */
-function sbpp04_dismiss_update_notice() {
-    update_user_option( get_current_user_id(), SBPP04_ADMIN_NOTIFICATION_HIDDEN_KEY, 1 );
-}
-add_action( 'wp_ajax_sbpp04_dismiss_update_notice', 'sbpp04_dismiss_update_notice' );
 
 /**
  * Setup admin option to allow users to hide from member directory.
